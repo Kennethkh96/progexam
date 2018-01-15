3cfdafe9d6d65404d56eebf934fd29a2a2dc8d00
Kenneth <kennethkh96@gmail.com>
2018-01-15 09:34:09 +0100

    now with server

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log("working");
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+app.set('port', (process.env.PORT || 3000));
+app.get('/', function (req, res) {
+    res.send("poato");
+});
+app.listen(app.get('port'));
--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log("working");
+import * as express from 'express';
+let app = express();
+app.set('port', (process.env.PORT || 3000));
+
+app.get('/', (req, res) => {
+    res.send("poato");
+});
+
+app.listen(app.get('port'));
